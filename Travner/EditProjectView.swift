b4ece1fdc288d5eddd83bea5f6cf53d284613bb1
Lorenzo Lins Mazzarotto <mazzardev@icloud.com>
2022-04-03 09:28:31 +0200

    Saving and deleting

--- Travner/EditProjectView.swift
+++ Travner/EditProjectView.swift
@@ -12,6 +12,9 @@ struct EditProjectView: View {
 
     @EnvironmentObject var dataController: DataController
 
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showingDeleteConfirm = false
+
     @State private var title: String
     @State private var detail: String
     @State private var color: String
@@ -65,18 +68,27 @@ struct EditProjectView: View {
                 }
 
                 Button("Delete this project") {
-                    // delete the project
+                    showingDeleteConfirm.toggle()
                 }
                 .accentColor(.red)
             }
         }
         .navigationTitle("Edit Project")
+        .onDisappear(perform: dataController.save)
+        .alert(isPresented: $showingDeleteConfirm) {
+            Alert(title: Text("Delete project?"), message: Text("Are you sure you want to delete this project? You will also delete all the items it contains."), primaryButton: .default(Text("Delete"), action: delete), secondaryButton: .cancel())
+        }
     }
 
     func update() {
         project.title = title
         project.detail = detail
         project.color = color
+    }
+
+    func delete() {
+        dataController.delete(project)
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
