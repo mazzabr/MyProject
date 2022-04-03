70022e03e8d1914fcd09ce16e63b3dfdf4067bbc
Lorenzo Lins Mazzarotto <mazzardev@icloud.com>
2022-04-03 08:01:06 +0200

    Initial Commit

---
+++ Travner/TravnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravnerApp.swift
+//  Travner
+//
+//  Created by Lorenzo Lins Mazzarotto on 03/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TravnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
